a6c60b287dc8f097854a1163e2a32c04d3700951
Joel <45838331+SenpaiBox@users.noreply.github.com>
2020-02-13 02:54:04 -0600

    Update install-mergerfs-debian9.sh

--- install-mergerfs-debian9.sh
+++ install-mergerfs-debian9.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 # Install Mergerfs
-if [[ -f "/usr/bin/mergerfs" ]]; then
+url="https://github.com/trapexit/mergerfs/releases/download/2.28.3"
+debian="mergerfs_2.28.3.debian-stretch_amd64.deb"
+if [ -f "/usr/bin/mergerfs" ]; then
     echo "INFO: $(date "+%m/%d/%Y %r") - Mergerfs already installed"
-    exit
 else
-    cd /tmp
-    curl https://github.com/trapexit/mergerfs/releases/download/2.28.3/mergerfs_2.28.3.debian-stretch_amd64.deb
-    dpkg -i mergerfs_2.28.3.debian-stretch_amd64.deb
+    mkdir -p ~/tmp
+    sudo wget -q ~/tmp/$debian $url/$debian
+    sudo dpkg -i ~/tmp/$debian
     echo "INFO: $(date "+%m/%d/%Y %r") - Mergerfs successfully installed"
     mergerfs -v
-    rm mergerfs_2.28.3.debian-stretch_amd64.deb
+    sudo find ~/tmp -name $debaian -delete >/dev/null
 fi
 exit
