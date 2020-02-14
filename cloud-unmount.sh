4fa1eb3e9b2a9d79372d8e12ab70f8cd81bf13b0
Joel <45838331+SenpaiBox@users.noreply.github.com>
2020-02-14 07:17:20 -0600

    add unmount

--- cloud-unmount.sh
+++ cloud-unmount.sh
@@ -10,6 +10,7 @@
 
 #### Configuration ####
 media="googlevps" # VPS share name NOTE: The name you want to give your share mount
+mediaroot="/mnt/user" # VPS share location
 
 #########################################
 #### DO NOT EDIT ANYTHING BELOW THIS ####
@@ -19,9 +20,11 @@ media="googlevps" # VPS share name NOTE: The name you want to give your share mo
 appdata="/mnt/user/appdata/rclonedata/$media" # Rclone data folder location NOTE: Best not to touch this or map anything here
 rcloneupload="$appdata/rclone_upload" # Staging folder of files to be uploaded
 rclonemount="$appdata/rclone_mount" # Rclone mount folder
+mergerfsmount="$mediaroot/$media" # Media share location
 
 # Unmount Rclone
 fusermount -u $rclonemount > /dev/null 2>&1
+fusermount -u $mergerfsmount > /dev/null 2>&1
 
 # Remove empty folders
 echo "INFO: $(date "+%m/%d/%Y %r") - STARTING UNMOUNT SCRIPT for \""${media}\"""
