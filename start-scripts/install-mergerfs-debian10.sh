6d26e5371ef257693b80b94bcc853b06cb96540a
Joel <45838331+SenpaiBox@users.noreply.github.com>
2020-02-18 15:38:17 -0600

    Update install-mergerfs-debian10.sh

--- start-scripts/install-mergerfs-debian10.sh
+++ start-scripts/install-mergerfs-debian10.sh
@@ -2,15 +2,23 @@
 # Mergerfs version: 2.28.3
 # This script installs mergerfs for "Debian 10 buster"
 
-url="https://github.com/trapexit/mergerfs/releases/download/2.28.3"
-pkg="mergerfs_2.28.3.debian-buster_amd64.deb"
+url="https://github.com/trapexit/mergerfs/releases/download/2.28.3/mergerfs_2.28.3.debian-buster_amd64.deb"
+mergerfs="/tmp/mergerfs.deb"
+if [ -f "/bin/fusermount" ]; then
+    printf "Fusermount already installed..."
+else
+    printf "Installing Fusermount..."
+    sudo apt update && sudo apt install fuse -y
+fi
 if [ -f "/usr/bin/mergerfs" ]; then
     printf "Mergerfs already installed..."
     exit
 else
-    wget -q ~/tmp/$pkg $url/$pkg & dpkg -i ~/tmp/$pkg
+    sudo curl -fsSL $url -o $mergerfs
+    sudo chmod +x $mergerfs
+    sudo dpkg -i $mergerfs
 fi
-find ~/tmp -name $pkg -delete >/dev/null
+sudo rm $mergerfs
 printf "Mergerfs successfully installed"
 mergerfs -v
 exit
