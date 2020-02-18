97f46b6b4ca37ae14569753a92cb3b21f0a5a09d
Joel <45838331+SenpaiBox@users.noreply.github.com>
2020-02-18 12:08:56 -0600

    Update install-docker-compose.sh

--- start-scripts/install-docker-compose.sh
+++ start-scripts/install-docker-compose.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 # This script will install latest version of "Docker-Compose"
 # Run after installing Docker
-command_exists() {
-    command -v "$@" > /dev/null 2>&1
-}
-user="$(id -un 2>/dev/null || true)"
-sh_c='sh -c'
-if [ "$user" != 'root' ]; then
-    if command_exists sudo; then
-        sh_c='sudo -E sh -c'
-        elif command_exists su; then
-        sh_c='su -c'
-    else
-			cat >&2 <<-'EOF'
-			Error: this installer needs the ability to run commands as root.
-			We are unable to find either "sudo" or "su" available to make this happen.
-			EOF
-        exit 1
-    fi
-fi
+
 if [ -f "/usr/bin/curl" ]; then
     echo "Curl already installed..."
 else
