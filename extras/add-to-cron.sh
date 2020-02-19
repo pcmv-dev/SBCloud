b20ffbb1d2192155273c7362bcd874026b04e8bb
Joel <45838331+SenpaiBox@users.noreply.github.com>
2020-02-19 04:18:03 -0600

    add to crontab script
    extra script

---
+++ extras/add-to-cron.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# This will add rclone scripts to crontab
+# You can only run this script once
+# To view your cronjobs type in terminal "crontab -l"
+# If you need to edit the times type in terminal "crontab -e"
+# Logs are located in "/mnt/user/logs"
+
+# CONFIGURE
+media="googlevps" # VPS share name NOTE: The name you want to give your share mount
+
+#########################################
+#### DO NOT EDIT ANYTHING BELOW THIS ####
+#########################################
+
+appdata="/mnt/user/rclonedata/$media"
+mkdir -p $appdata
+if [ -f "$appdata/cron_job_added" ]; then
+    echo "INFO: $(date "+%m/%d/%Y %r") - Cronjob already added please edit with \""crontab -e\"""
+    exit
+else
+    echo "INFO: $(date "+%m/%d/%Y %r") - Added rclone scripts to crontab"
+    touch $appdata/cron_job_added
+    (crontab -l 2>/dev/null; echo "# Rclone scripts for \""${media}\""") | crontab -
+    (crontab -l 2>/dev/null; echo "0 */1 * * * $HOME/VPSCloudStorage/rclone/vps-mount.sh > /mnt/user/logs/vps-mount.log >/dev/null 2>&1") | crontab -
+    (crontab -l 2>/dev/null; echo "*/15 * * * * $HOME/VPSCloudStorage/rclone/vps-upload.sh > /mnt/user/logs/vps-upload.log >/dev/null 2>&1") | crontab -
+    (crontab -l 2>/dev/null; echo "@reboot $HOME/VPSCloudStorage/rclone/vps-unmount.sh > /mnt/user/logs/vps-mount.log >/dev/null 2>&1") | crontab -
+fi
+crontab -l
+exit
