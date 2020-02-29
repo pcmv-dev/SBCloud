de60704d65d1f7e4c278fd784e8ade3250f3b584
Joel <45838331+SenpaiBox@users.noreply.github.com>
2020-02-29 05:43:54 -0600

    user prompt to update
    ask for user input to update

--- install-scripts/install-docker-compose.sh
+++ install-scripts/install-docker-compose.sh
@@ -6,13 +6,22 @@ dockercompose="/usr/local/bin/docker-compose"
 compose_ver="$(curl -s -o /dev/null -I -w "%{redirect_url}\n" https://github.com/docker/compose/releases/latest | grep -oP "[0-9]+(\.[0-9]+)+$")"
 compose_url="https://github.com/docker/compose/releases/download/$compose_ver/docker-compose-$(uname -s)-$(uname -m)"
 if [ -f "$dockercompose" ]; then
-    echo "Docker-Compose is already Insalled"
-    docker-compose --version
-    exit 1
+    echo "docker-compose is installed..."
+    echo -n "Install/Update anyway (y/n)? "
+    read answer
+    if [ "$answer" != "${answer#[Yy]}" ]; then
+        sudo rm -rf $dockercompose
+        sudo curl -fsSL $compose_url -o $dockercompose
+        sudo chmod +x $dockercompose
+        echo "docker-compose successfully installed"
+        docker-compose --version
+    else
+        exit
+    fi
 else
     sudo curl -fsSL $compose_url -o $dockercompose
     sudo chmod +x $dockercompose
-    echo "Docker-Compose successfully installed"
+    echo "docker-compose successfully installed"
     docker-compose --version
 fi
 exit
