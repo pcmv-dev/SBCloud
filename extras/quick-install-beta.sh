aa9b7736c5f2a8fd925da5461b1102ef20a6fcc4
Joel <45838331+SenpaiBox@users.noreply.github.com>
2020-03-01 05:08:23 -0600

    Update quick-install-beta.sh

--- extras/quick-install-beta.sh
+++ extras/quick-install-beta.sh
@@ -3,13 +3,13 @@
 # Autosetup of "CloudStorage"
 
 # Install needed packages
-sudo apt update && sudo apt install git p7zip-full fuse -y
+apt update && apt install git p7zip-full fuse -y
 
 # Install Rclone
 if [ -f "/usr/bin/rclone" ]; then
     echo "Rclone already installed..."
 else
-    curl https://rclone.org/install.sh | sudo bash -s beta >/dev/null
+    curl https://rclone.org/install.sh |  bash -s beta >/dev/null
 fi
 
 # Install Mergerfs
@@ -24,7 +24,7 @@ if [ -f "/usr/bin/mergerfs" ]; then
     echo -n "Install/Update anyway (y/n)? "
     read answer
     if [ "$answer" != "${answer#[Yy]}" ]; then
-        sudo rm -rf /usr/bin/mergerfs
+        rm -rf /usr/bin/mergerfs
         curl -fsSL $url -o $mergerfs
         sudo chmod +x $mergerfs
         sudo dpkg -i $mergerfs
@@ -34,7 +34,7 @@ else
     sudo chmod +x $mergerfs
     sudo dpkg -i $mergerfs
 fi
-sudo rm $mergerfs >/dev/null 2>&1
+rm $mergerfs >/dev/null 2>&1
 
 # Install Docker
 if [ -x "$(command -v docker)" ]; then
@@ -43,14 +43,14 @@ if [ -x "$(command -v docker)" ]; then
     echo -n "Run anyway (y/n)? "
     read docker
     if [ "$docker" != "${docker#[Yy]}" ]; then
-        sudo rm -f /mnt/user/cloudstorage/install-scripts/install-docker.sh
-        sudo curl -fsSL https://get.docker.com -o /mnt/user/cloudstorage/install-scripts/install-docker.sh
+        rm -f /mnt/user/cloudstorage/install-scripts/install-docker.sh
+        curl -fsSL https://get.docker.com -o /mnt/user/cloudstorage/install-scripts/install-docker.sh
         sh /mnt/user/cloudstorage/install-scripts/install-docker.sh
     fi
 else
     mkdir -p /mnt/user/cloudstorage/install-scripts
-    sudo curl -fsSL https://get.docker.com -o /mnt/user/cloudstorage/install-scripts/install-docker.sh
-    sudo sh /mnt/user/cloudstorage/install-scripts/install-docker.sh
+    curl -fsSL https://get.docker.com -o /mnt/user/cloudstorage/install-scripts/install-docker.sh
+    sh /mnt/user/cloudstorage/install-scripts/install-docker.sh
 fi
 
 # Install docker-compose
@@ -64,17 +64,17 @@ if [ -f "$dockercompose" ]; then
     read answer
     if [ "$answer" != "${answer#[Yy]}" ]; then
         rm -rf $dockercompose
-        sudo curl -fsSL $compose_url -o $dockercompose
+        curl -fsSL $compose_url -o $dockercompose
         sudo chmod +x $dockercompose
     fi
 else
-    sudo curl -fsSL $compose_url -o $dockercompose
+    curl -fsSL $compose_url -o $dockercompose
     sudo chmod +x $dockercompose
 fi
 
 # Install Portainer
 container="portainer"
-if sudo docker ps -a --format '{{.Names}}' | grep -Eq "^${container}\$"; then
+if  docker ps -a --format '{{.Names}}' | grep -Eq "^${container}\$"; then
     echo "Portainer already installed..."
 else
     echo "Installing Portainer..."
@@ -91,18 +91,18 @@ if [ -f "$PATH/.update" ]; then
     echo -n "Download and replace current scripts (y/n)? "
     read rclonescripts
     if [ "$rclonescripts" != "${rclonescripts#[Yy]}" ]; then
-        sudo rm -f $PATH/rclone-mount.sh && sudo rm -f $PATH/rclone-unmount.sh && sudo rm -f $PATH/rclone-upload.sh
-        sudo curl -fsSL https://raw.githubusercontent.com/SenpaiBox/CloudStorage/master/rclone/rclone-mount.sh -o $PATH/rclone-mount.sh
-        sudo curl -fsSL https://raw.githubusercontent.com/SenpaiBox/CloudStorage/master/rclone/rclone-unmount.sh -o $PATH/rclone-unmount.sh
-        sudo curl -fsSL https://raw.githubusercontent.com/SenpaiBox/CloudStorage/master/rclone/rclone-upload.sh -o $PATH/rclone-upload.sh
+        rm -f $PATH/rclone-mount.sh &&  rm -f $PATH/rclone-unmount.sh &&  rm -f $PATH/rclone-upload.sh
+        curl -fsSL https://raw.githubusercontent.com/SenpaiBox/CloudStorage/master/rclone/rclone-mount.sh -o $PATH/rclone-mount.sh
+        curl -fsSL https://raw.githubusercontent.com/SenpaiBox/CloudStorage/master/rclone/rclone-unmount.sh -o $PATH/rclone-unmount.sh
+        curl -fsSL https://raw.githubusercontent.com/SenpaiBox/CloudStorage/master/rclone/rclone-upload.sh -o $PATH/rclone-upload.sh
     else
         exit
     fi
 else
-    sudo touch $PATH/.update
-    sudo curl -fsSL https://raw.githubusercontent.com/SenpaiBox/CloudStorage/master/rclone/rclone-mount.sh -o $PATH/rclone-mount.sh
-    sudo curl -fsSL https://raw.githubusercontent.com/SenpaiBox/CloudStorage/master/rclone/rclone-unmount.sh -o $PATH/rclone-unmount.sh
-    sudo curl -fsSL https://raw.githubusercontent.com/SenpaiBox/CloudStorage/master/rclone/rclone-upload.sh -o $PATH/rclone-upload.sh
+    touch $PATH/.update
+    curl -fsSL https://raw.githubusercontent.com/SenpaiBox/CloudStorage/master/rclone/rclone-mount.sh -o $PATH/rclone-mount.sh
+    curl -fsSL https://raw.githubusercontent.com/SenpaiBox/CloudStorage/master/rclone/rclone-unmount.sh -o $PATH/rclone-unmount.sh
+    curl -fsSL https://raw.githubusercontent.com/SenpaiBox/CloudStorage/master/rclone/rclone-upload.sh -o $PATH/rclone-upload.sh
 fi
 
 # Install complete
@@ -115,7 +115,7 @@ docker -v
 docker-compose --version
 echo
 echo "Install complete! Now do the following:"
-echo "1.) Run 'sudo usermod -aG docker USER' to run docker without root, change 'USER' to your own"
-echo "2.) Run 'sudo chmod -R +x /mnt/user && sudo chown -R USER:USER /mnt/user', change 'USER' to your own"
+echo "1.) Run ' usermod -aG docker USER' to run docker without root, change 'USER' to your own"
+echo "2.) Run ' chmod -R +x /mnt/user &&  chown -R USER:USER /mnt/user', change 'USER' to your own"
 echo "3.) Relog"
 exit
