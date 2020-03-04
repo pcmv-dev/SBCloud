96c99a7f53bc8db3c66698230a02ef5d0cfc62e0
Joel <45838331+SenpaiBox@users.noreply.github.com>
2020-03-04 00:34:16 -0600

    Update add-to-cron.sh

--- extras/add-to-cron.sh
+++ extras/add-to-cron.sh
@@ -5,35 +5,34 @@
 # To delete all cron tasks type in terminal "crontab -r"
 # Logs are located in "/mnt/logs"
 
+if [ `whoami` = root ]; then
+    echo "Do not run as sudo/root!"
+    exit
+fi
 tee <<-EOF
 
-Rclone Cronjob Tasks
+Add Rclone scripts to crontab
 
 [1] Add Rclone scripts to crontab
-[2] Remove all cronjobs in crontab
+[2] Reset crontab
 
-[E] Exit
+[3] Exit
 
 EOF
 
-if [ `whoami` = root ]; then
-    echo "Do not run as sudo/root! Will now exit..."
-    exit
-fi
-APPDATA="/mnt/cloudstorage/rclone" # Location of your Rclone scripts
 read -p 'Type a Number | Press [ENTER]: ' typed </dev/tty
-if [ "$typed" == "1" ]; then
+if [ "$typed" -eq "1" ]; then
     (crontab -l 2>/dev/null; echo "") | crontab -
     (crontab -l 2>/dev/null; echo "# Rclone scripts") | crontab -
-    (crontab -l 2>/dev/null; echo "@hourly $APPDATA/rclone-mount > /mnt/logs/rclone-mount.log 2>&1") | crontab -
-    (crontab -l 2>/dev/null; echo "*/15 * * * * $APPDATA/rclone-upload > /mnt/logs/rclone-upload.log 2>&1") | crontab -
-    (crontab -l 2>/dev/null; echo "@reboot $APPDATA/rclone-unmount > /mnt/logs/rclone-unmount.log 2>&1") | crontab -
+    (crontab -l 2>/dev/null; echo "@hourly /mnt/cloudstorage/rclone/rclone-mount > /mnt/logs/rclone-mount.log 2>&1") | crontab -
+    (crontab -l 2>/dev/null; echo "*/15 * * * * /mnt/cloudstorage/rclone/rclone-upload 2>&1") | crontab -
+    (crontab -l 2>/dev/null; echo "@reboot /mnt/cloudstorage/rclone/rclone-unmount > /mnt/logs/rclone-unmount.log 2>&1") | crontab -
     echo
     crontab -l
     /etc/init.d/cron reload
-    elif [ "$typed" == "2" ]; then
+    elif [ "$typed" -eq "2" ]; then
     crontab -r -i
-    elif [ "$typed" == "E" ] || [ "$typed" == "e" ]; then
+    elif [ "$typed" = "3" ]; then
     exit
 fi
 exit
