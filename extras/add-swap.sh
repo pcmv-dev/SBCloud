f045405c5327b7bbdb80104c91c4195f650debac
Joel <45838331+SenpaiBox@users.noreply.github.com>
2020-03-07 02:07:16 -0600

    Update add-swap.sh

--- extras/add-swap.sh
+++ extras/add-swap.sh
@@ -11,7 +11,6 @@ grep -q "swapfile" /etc/fstab
 if [ $? -ne 0 ]; then
   echo "Swapfile not found. Adding swapfile"
   fallocate -l ${swapsize} /swapfile
-  sudo dd if=/dev/zero of=/swapfile bs=1024 count=1048576
   chmod 600 /swapfile
   mkswap /swapfile
   swapon /swapfile
